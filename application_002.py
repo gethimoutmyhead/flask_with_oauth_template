d44e2815a38f206d7fd915bc11e230e05b180f02
gethimoutmyhead <theshashiman@gmail.com>
2026-06-12 11:58:51 +0800

    successfully authenticates and issues a refresh token, access token and id token

--- application_002.py
+++ application_002.py
@@ -16,11 +16,6 @@ oidcserver_metadataURL = f"{env['oidc_authserver']}/.well-known/openid-configura
 oidcServer_client = OAuth2Session(
 	client_id=env['oidc_clientID'],
 	client_secret=env['oidc_clientSecret'],
-	server_metadata_url=oidcserver_metadataURL,
-	client_kwargs={
-		"scope": "openid profile",
-		"token_endpoint_auth_method": "client_secret_post",
-	}
 )
 
 @app.route("/")
@@ -30,18 +25,23 @@ def hello():
 @app.route("/login")
 def login():
 	url_redirectAfterAuth = url_for('oidc_server_callback', _external=True)
-	loginURI, state = oidcServer_client.create_authorization_url(url=f"{env['oidc_authserver']}/authorize",redirect_uri=url_redirectAfterAuth)
+	loginURI, state = oidcServer_client.create_authorization_url(
+		url=f"{env['oidc_authserver']}/authorize",
+		redirect_uri=url_redirectAfterAuth,
+		response_type='code',
+		scope='openid profile offline_access',
+	)
 	session['oidc_state'] = state
 	return redirect(loginURI)
 
 @app.route("/after-authentication")
 def oidc_server_callback():
-
 	token_endpoint = f"{env['oidc_authserver']}/oauth/token"
-	jojo = oidcServer_client.fetch_token(token_endpoint, 
+	oidc_token = oidcServer_client.fetch_token(token_endpoint,
 		authorization_response=request.url, 
-		redirect_uri=url_for('oidc_server_callback', _external=True))
-	return f'{jojo}'
+		redirect_uri=url_for('oidc_server_callback', _external=True),
+	)
+	return f'{oidc_token}'
 
 @app.route('/logged-in')
 def logged_in():
