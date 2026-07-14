20b873b2fa72149bcfa5faffc41e914e79d07fd5
gethimoutmyhead <theshashiman@gmail.com>
2026-07-14 08:40:41 +0800

    more test code for authz

--- readme.md
+++ readme.md
@@ -5,6 +5,8 @@ template set up to allow me to make a flask app that has authentication built in
 Main branch auth0-oidc is set up to work as an auth0 client
 
 ## Completed
+2026-07-13
+* set up tests that confirm authserver_token cookies that are malformed will be rejected
 2026-07-10
 * set up tests for token cookies that are invalid
 * page now has a system to validate access token, identity token. it needs more extensive testing, but will check necessary claims, identify if the audience is incorrect. adding code to check roles should be easy, its all functional now huzzah
@@ -15,7 +17,6 @@ Main branch auth0-oidc is set up to work as an auth0 client
 * add access token validation to flask App (can lift this from the test function)
 
 ## Things to do
-* set up granular tests that check id_token, access_token validity
 * authorization tests that utilise permittedRoles, permittedAttributes
 * authorization code that uses permittedRoles and permittedAttributes
 * template functions that allow user metadata, app metadata modifications
@@ -23,3 +24,8 @@ Main branch auth0-oidc is set up to work as an auth0 client
 * push form that allows user creation
 * script that creates new, or updates existing .env files (makes it easier to deploy anywhere)
 * script to configure gunicorn.conf.py
+* tests to monitor behavior of access token, id token tests that are present but tampered
+* tests to monitor behavior of access token, id token tests that are present but have claims missing
+* tests to monitor behavior of access token, id token tests that are present but expired
+
+
--- test/test_authenticatedUser.py
+++ test/test_authenticatedUser.py
@@ -150,13 +150,59 @@ def test_accessAuthzPageWithAuthn():
 	assert page.status_code == 200, f'expected page to load, got {page.status_code}\n {page.headers}'
 
 
+tokensToTest = [
+	{
+		'test_token': {'nauthserver_token': 'someNonsense'},
+		'expected_error_message': 'AppAuthzERROR: authserver token not present'
+	},
+	{
+		'test_token': {
+			'authserver_token':{
+				'id_token':'whisky',
+				}
+			},
+		'expected_error_message': 'AppAuthzERROR: access token not present'
+	},
+	{
+		'test_token': {
+			'authserver_token':{
+				'access_token':'whisky',
+				}
+			},
+		'expected_error_message': 'AppAuthzERROR: id token not present'
+	},
+	{
+		'test_token': {
+			'authserver_token':{
+				'access_token':'whisky',
+				'id_token':'malt',
+				}
+			},
+		'expected_error_message': 'AppAuthzERROR: id token failed to decode'
+	},
+]
 @pytest.mark.order(7)
-def test_accessAuthzPageWithInvalidTokens():
-	target_url=f"https://{flaskAppSettings['app_server_url']}/authenticated"
-	cookies = {"authserver_token": 'someNonsense'}
- 
-	response = requests.get(target_url, cookies=cookies, allow_redirects=False,verify=flaskAppSettings['publicCert_site'])
- 
+@pytest.mark.parametrize("tokenToTest", tokensToTest)
+def test_accessAuthzPageWithInvalidTokens(tokenToTest):
+	try:
+		target_url=f"https://{flaskAppSettings['app_server_url']}/authenticated"
+		tokenAsFlaskCookie = flaskCookieMaker.encodeFlaskCookie(flaskAppSettings['app_cookieSigning_secret'],tokenToTest['test_token'])
+		sessionCookie = requests.cookies.create_cookie(
+				name='session',
+				value=tokenAsFlaskCookie,
+				domain='127.0.0.1',
+				path='/',
+				secure=True,
+				rest={'HttpOnly': True}
+			)
+		testSession = requests.Session()
+		testSession.cookies.set_cookie(sessionCookie)
+
+	except Exception as e:
+		pytest.fail(f"unknown exception, tokenToTest is {tokenToTest['test_token']}\n {e} \n {tokenAsFlaskCookie} \n {sessionCookie}")
+
+	response = testSession.get(f"https://{flaskAppSettings['app_server_url']}/authenticated",verify=flaskAppSettings['publicCert_site'], allow_redirects=False)
+
 	# 1. Assert the response is a redirect.
 	assert response.status_code in [301, 302, 303, 307, 308], (
 		f"Expected a redirect status code (one of [301, 302, 303, 307, 308]), "
@@ -170,8 +216,8 @@ def test_accessAuthzPageWithInvalidTokens():
 		f"Headers received: {dict(response.headers)}"
 	)
  
-	assert response.headers["X-Error-Message"] == "AppAuthzERROR: authserver token not present", (
-		f"Expected X-Error-Message to be 'authentication token missing', "
+	assert tokenToTest['expected_error_message'] in response.headers["X-Error-Message"], (
+		f"Expected X-Error-Message to be {tokenToTest['expected_error_message']}, "
 		f"got {response.headers['X-Error-Message']!r} instead."
 	)
  
